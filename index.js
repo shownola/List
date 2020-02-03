d9b0d4ec70fec50469e1bcea8e966909efbf56a5
shownola <swasieko@gmail.com>
2020-02-03 15:17:48 -0700

    Callback-based functions using promises

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ const util = require('util');
 
 
 // METHOD #2
-const lstat = util.promisify(fs.lstat);
+// const lstat = util.promisify(fs.lstat);
 
+// METHOD #3
+const lstat = fs.promises.lstat;
+
+
+// or using destructing can do the following:
+// const { lstat } = fs.promises;
 
 // EITHER: err === an error object, which means something went wrong or err === null, which means everything is OK
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if(err){
     console.log(err);
   }
+  for(let filename of filenames){
+    try {
+      const stats = await lstat(filename);
 
-
-
+      console.log(filename, stats.isFile());
+    } catch(err){
+      console.log(err);
+    }
+  }
 });
 
 
