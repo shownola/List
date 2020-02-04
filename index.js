76682b124cb19357e7d5a85894c82a592b484c52
shownola <swasieko@gmail.com>
2020-02-03 17:58:53 -0700

    Implemented ability accept command line arguments and to join paths

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const util = require('util');
 const chalk = require('chalk');
+const path = require('path');
 
 
 // METHOD #2
@@ -16,15 +17,17 @@ const lstat = fs.promises.lstat;
 
 // EITHER: err === an error object, which means something went wrong or err === null, which means everything is OK
 
-console.log(process.argv);
+// console.log(process.argv);
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if(err){
     console.log(err);
   }
 
   const statPromises = filenames.map(filename => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
   const allStats = await Promise.all(statPromises);
 
@@ -32,12 +35,12 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     const index = allStats.indexOf(stats);
 
     if(stats.isFile()){
-      console.log(filename[index]);
+      console.log(filenames[index]);
     } else {
       console.log(chalk.bold(filenames[index]));
     }
 
-    console.log(filenams[index], stats.isFile());
+    console.log(filenames[index], stats.isFile());
   }
 });
 
